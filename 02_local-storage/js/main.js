f736b4180cbd0489e9a8c2be7e4ff00f436bd92f
Chris Henrick <chrishenrick@gmail.com>
2013-10-16 11:26:41 -0400

    updated css and js

--- 02_local-storage/js/main.js
+++ 02_local-storage/js/main.js
@@ -1,5 +1,5 @@
 
-// create a layer for stamen basemaps
+// create a variable to load Stamen 'toner' tiles
 var layer = new L.StamenTileLayer("toner");
 
 // initialize and set map center and zoom
@@ -8,11 +8,41 @@ var map = L.map('map', {
 	zoom: 12
 });
 
+// create the map 
 map.addLayer(layer);
 
-// add base map tiles
-// L.tileLayer('')
+// grab the processed GeoJSON through ajax call
+var geojsonFeature = (function() {
+        var json = null;
+        $.ajax({
+            'async': false,
+            'global': false,
+            'url': "/data/test_random.json",
+            'dataType': "json",
+            'success': function (data) {
+                json = data;
+            }
+        });
+        return json;
+    })();
 
-// add GeoJSON layer *** need to figure out how to convert plain JSON to GeoJSON ***
-var myLayer = L.geoJson().addTo(map);
-	myLayer.addData(data/test.json);
+// create an object to store marker style properties
+var geojsonMarkerOptions = {
+    radius: 10,
+    fillColor: "rgb(255,0,195)",
+    color: "#000",
+    weight: 0,
+    opacity: 1,
+    fillOpacity: 1
+};
+
+// load the geojson to the map
+// var myLayer = L.geoJson().addTo(map);
+// 	myLayer.addData(json);
+
+// load the geojson to the map with marker styling
+L.geoJson(geojsonFeature, {
+	pointToLayer: function (feature, latlng) {
+		return L.circleMarker(latlng, geojsonMarkerOptions)
+	}
+}).addTo(map);
