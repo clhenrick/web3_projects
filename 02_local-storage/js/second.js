90600d3fa356becbb399ccc7710703d65479467c
Chris Henrick <chrishenrick@gmail.com>
2013-10-21 22:51:36 -0400

    updated js to include demo animation with polylines

--- 02_local-storage/js/second.js
+++ 02_local-storage/js/second.js
@@ -49,24 +49,24 @@ var geojsonMarkerOptions = {
 
 // create pop-ups for markers
 function onEachFeature(feature, layer) {
-        var popupContent,
-            popupOptions = geojsonMarkerOptions;
+  var popupContent,
+      popupOptions = geojsonMarkerOptions;
 
-        popupContent = feature.properties.t;
+  popupContent = feature.properties.t;
 
-        // create a new variable to store Date in
-        var time = new Date(0);
-        // create a date by passing it the Unix UTC epoch
-        time.setUTCSeconds(popupContent);
+  // create a new variable to store Date in
+  var time = new Date(0);
+  // create a date by passing it the Unix UTC epoch
+  time.setUTCSeconds(popupContent);
 
-        // make the init popup content time
-        popupContent = time;
+  // make the init popup content time
+  popupContent = time;
 
-        // console.log(popupContent);	
+  // console.log(popupContent);	
 
-        layer.bindPopup("<b>Time:</b> " + time +
-	                    "<br><b>Altitude: </b>" + feature.properties.alt + "meters"
-	                    ,popupOptions);
+  layer.bindPopup("<b>Time:</b> " + time +
+                "<br><b>Altitude: </b>" + feature.properties.alt + "meters"
+                ,popupOptions);
 }
 
 // NOT YET WORKING...
@@ -74,30 +74,35 @@ function onEachFeature(feature, layer) {
 // polyLines created from pairs of latlons that share matching 't' value
 function createLineFeatures(feature, feature_two) {
 
-      console.log('called createLineFeatures');
+  console.log('called createLineFeatures');
 
-      // create a for loop to grab the first features lat lon
-      for (i = 0; i < feature.length; i++){
+  // create a for loop to grab the first features lat lon
+  for (i = 0; i < feature.length; i++){
 
-        //console.log(feature[i].properties.t);
+    //console.log(feature[i].properties.t);
 
-        for (j = 0; j < feature_two.length; j++){ 
+    for (j = 0; j < feature_two.length; j++){ 
 
-          //console.log(feature_two[j].properties.t);
-          console.log(feature_two[j].geometry.coordinates);
+      //console.log(feature_two[j].properties.t);
+      //console.log(feature_two[j]);
 
-          // if the unix time is the same in both features
-          if (feature[i].properties.t === feature_two[j].properties.t) {
+      // if the unix time is the same in both features
+      if (feature[i].properties.t == feature_two[j].properties.t) {
 
-          var polyline = L.polyline([[feature[i].geometry.coordinates], [feature_two[j].geometry.coordinates]])
-          //console.log(polyline);
+        console.log(feature[i].geometry.coordinates + feature_two[j].geometry.coordinates);
 
-          myData.polylinesTest.push(polyline);
+        var coordinatesA = feature[i].geometry.coordinates,
+            coordinatesB = feature_two[j].geometry.coordinates,
+            polyline = L.polyline([[coordinatesA], [coordinatesB]]);
+        
+        console.log(polyline); // returns weird `e` values
 
-        } // end if
-      
-      } // end inner for loop
+        myData.polylinesTest.push(polyline);
+
+      } // end if
   
+    } // end inner for loop
+
   } // end outer for loop
 
 } // end createLineFeatures
@@ -156,28 +161,33 @@ function createMarkers(input_data) {
 }
 
 
-var scrambledGeo = createMarkers(myData.scrambled);
-var origGeo = createMarkers(myData.original);
+//var scrambledGeo = createMarkers(myData.scrambled);
+//var origGeo = createMarkers(myData.original);
 
 setTimeout(function() {
-  createLineFeatures(myData.dataOrig, myData.dataNew)
+ // createLineFeatures(myData.dataOrig, myData.dataNew)
 }, 2000);
 
 /*********************************************************************
  * create animated markers
 *********************************************************************/
 
-// not yet working
+// what the polylines should look like...
+var routelines = [
+  L.polyline([[40.66893768310547,-73.97364044189453],[40.700906724998084,-73.982159924856]]),
+  L.polyline([[40.66240692138672,-73.96121215820312],[40.67959172823107,-73.93065222433161]]),
+  L.polyline([[40.666404724121094,-73.96212005615234],[40.6492585313864,-73.92650879888541]]),
+  L.polyline([[40.70763397216797,-73.96053314208984],[40.68801800482941,-73.89643122463292]]),
+  L.polyline([[40.72077941894531,-73.96383666992188],[40.69775273343673,-73.91169387790706]]),
+  L.polyline([[40.701236724853516,-73.96379852294922],[40.738112456274926,-73.99835990747752]])
+]
 
-  $.each(myData.polylinesTest, function(i, routeLine) {
+  $.each(routelines, function(i, routeLine) {
     var marker = L.animatedMarker(routeLine.getLatLngs(), {
       //icon: bikeIcon,
       autoStart: false,
       onEnd: function() {
         $(this._shadow).fadeOut();
-        $(this._icon).fadeOut(3000, function(){
-          map.removeLayer(this);
-        });
       }
     });
 
